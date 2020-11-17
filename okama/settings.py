5ceff936cdf2882eeb5d583133b3bf4f5bd047ed
Sergey Kikevich <sergey@rostsber.ru>
2020-11-17 08:26:24 +0300

    cleaning settings.py

--- okama/settings.py
+++ okama/settings.py
@@ -2,14 +2,7 @@ default_ticker = 'SPY.US'
 default_tickers_list = ['SPY.US', 'BND.US']  # required in frontier.py
 default_macro = 'RUB.INFL'
 
-EOD_url = 'https://eodhistoricaldata.com/api/eod/'
-EOD_div_url = 'https://eodhistoricaldata.com/api/div/'
-EOD_live = 'https://eodhistoricaldata.com/api/real-time/'
-eod_search_url = 'https://eodhistoricaldata.com/api/search/'
-eod_exchanges_url = 'https://eodhistoricaldata.com/api/exchanges/'
-
 # Namespaces
-#
 assets_namespaces = ('US', 'XETR', 'MOEX', 'PIF', 'FX', 'INDX', 'COMM', 'RE')
 macro_namespaces = ('INFL', 'RATE')
 
