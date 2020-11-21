87945afb7696bd905cea2815a74bb52d78f312e8
Sergey Kikevich <sergey@rostsber.ru>
2020-11-21 14:32:30 +0300

    v0.86

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='okama',
-    version='0.85',
+    version='0.86',
     license='MIT',
     description='Modern Portfolio Theory (MPT) Python package',
     long_description=long_description,
