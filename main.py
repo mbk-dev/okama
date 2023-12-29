b73342c80d748da3a9facfd1c2d0e91a3896f7d4
Sergey Kikevich <sergey@rostsber.ru>
2023-12-29 18:48:54 +0300

    chore: catch en error in EF

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 import okama as ok
 
-indexes = ["RGBITR.INDX", "MCFTR.INDX", "GC.COMM"]
-ef = ok.EfficientFrontier(indexes, ccy="RUB", full_frontier=True, inflation=False, n_points=50)
-ef.plot_cml(rf_return=0.15, y_axe="cagr")
-plt.show()
+indexes = ["RGBITR.INDX", "RUCBTRNS.INDX", "MCFTR.INDX", "GC.COMM"]
+bounds = [(0, 0.7), (0, 0.7), (0, 1), (0, 0.19)]
+ef = ok.EfficientFrontier(indexes, ccy="RUB", full_frontier=False, bounds=bounds, inflation=False)
+print(ef.get_tangency_portfolio(rf_return=0.08))
+## TODO: replace ValueError: Weights sum is not equal to one. with custom error mesage
 
-pf = ok.Portfolio(["SPY.US", "BND.US"], weights=[.5, .5], rebalancing_period="month")
-pf.wealth_index_with_assets.plot()
-plt.show()
