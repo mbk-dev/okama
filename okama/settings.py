a7e771f67fcc209a0b86c176c8e23481288b12d7
Sergey Kikevich <sergey@rostsber.ru>
2024-02-21 18:33:12 +0300

    feat: add discount rate default value to settings

--- okama/settings.py
+++ okama/settings.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 
+# tickers
 default_ticker = "SPY.US"
 default_namespace = "US"
 default_tickers_list = ["SPY.US", "BND.US"]  # required in single_period.py
@@ -8,6 +9,8 @@ default_tickers_list = ["SPY.US", "BND.US"]  # required in single_period.py
 default_macro_inflation = "RUB.INFL"
 default_macro_rate = "RUS_CBR.RATE"
 default_macro_indicator = "USA_CAPE10.RATIO"
+
+DEFAULT_DISCOUNT_RATE = 0.05
 
 _MONTHS_PER_YEAR = 12
 PeriodLength = namedtuple("PeriodLength", "years months")
