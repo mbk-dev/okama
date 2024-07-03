fix: ror calculation in 0 period for Rebalanced portfolios

okama/common/helpers/helpers.py
@@ -404,6 +404,7 @@ class Frame:
             kstest = scipy.stats.kstest(ror, distr, scipy.stats.norm.fit(ror))
         elif distr == "lognorm":
             kstest = scipy.stats.kstest(ror, distr, scipy.stats.lognorm.fit(ror))
+            # TODO: add Student's t distribution
         else:
             raise ValueError('distr should be "norm" (default) or "lognormal".')
         return {"statistic": kstest[0], "p-value": kstest[1]}
@@ -505,7 +506,7 @@ class Rebalance:
         """
         # define data of the first period
         first_date = ror.index[0]
-        return_first_period = ror.iloc[0] @ weights / ror.shape[0]
+        return_first_period = ror.iloc[0] @ weights
 
         wealth_index = self.wealth_ts(weights, ror)
         ror = wealth_index.pct_change()

okama/portfolio.py
@@ -2175,6 +2175,7 @@ class Portfolio(make_asset_list.ListMaker):
             std, loc, scale = scipy.stats.lognorm.fit(data)
             mu = np.log(scale)
             p = scipy.stats.lognorm.pdf(x, std, loc, scale)
+            # TODO: add Student's t distribution
         else:
             raise ValueError('distr must be "norm" (default) or "lognorm".')
         plt.plot(x, p, "k", linewidth=2)
@@ -2335,7 +2336,7 @@ class PortfolioDCF:
 
         Accumulated inflation time series is added if `inflation=True` in the Portfolio.
 
-        If there is no cashflows Wealth index is obtained from the accumulated return multiplicated
+        If there are no cashflows, Wealth index is obtained from the accumulated return multiplicated
         by the initial investments. That is: initial_amount_pv * (Acc_Return + 1)
 
         initial_amount_pv is the discounted value of the initial investments (initial_amount).
@@ -2649,7 +2650,7 @@ class PortfolioDCF:
         >> s.quantile(50 / 100)
         2.7
         """
+        # TODO: self._monte_carlo_wealth should be cached
         s2 = self._monte_carlo_wealth(first_value=self.parent.initial_amount, distr=distr, years=years, n=n)
         dates: pd.Series = helpers.Frame.get_survival_date(s2)
-
         return dates.apply(helpers.Date.get_period_length, args=(self.parent.last_date,))