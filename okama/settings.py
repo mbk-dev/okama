0b558751717a3ffcc6447003d7ed1bbeda4b1c64
Sergey Kikevich <sergey@rostsber.ru>
2024-09-17 09:24:34 +0300

    feat: DEFAULT_INITIAL_INVESTMENT is now a constant in settings

--- okama/settings.py
+++ okama/settings.py
@@ -11,6 +11,7 @@ default_macro_rate = "RUS_CBR.RATE"
 default_macro_indicator = "USA_CAPE10.RATIO"
 
 DEFAULT_DISCOUNT_RATE = 0.05
+DEFAULT_INITIAL_INVESTMENT = 1000.
 
 _MONTHS_PER_YEAR = 12
 PeriodLength = namedtuple("PeriodLength", "years months")
