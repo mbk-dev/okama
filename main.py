5e8a350603e24e61979324c9239db3833e8c27b3
Sergey Kikevich <sergey@rostsber.ru>
2024-10-11 10:44:49 +0300

    chore: update main.py

--- main.py
+++ main.py
@@ -18,18 +18,18 @@ pf = ok.Portfolio(l, weights=w, ccy="EUR", rebalancing_period="none")
 
 
 
-# # Fixed Percentage strategy
-# pc = ok.PercentageStrategy(pf)
-# pc.initial_investment = 10_000
-# pc.frequency = "year"
-# pc.percentage = -0.08
+# Fixed Percentage strategy
+pc = ok.PercentageStrategy(pf)
+pc.initial_investment = 10_000
+pc.frequency = "year"
+pc.percentage = -0.08
 
-# Fixed Amount strategy
-ind = ok.IndexationStrategy(pf)
-ind.initial_investment = 10_000
-ind.frequency = "year"
-ind.amount = -500
-ind.indexation = "inflation"
+# # Fixed Amount strategy
+# ind = ok.IndexationStrategy(pf)
+# ind.initial_investment = 10_000
+# ind.frequency = "year"
+# ind.amount = -500
+# ind.indexation = "inflation"
 
 # # TimeSeries strategy
 # d = {
@@ -42,16 +42,23 @@ ind.indexation = "inflation"
 # ts.time_series_dic = d
 
 # Assign a strategy
-pf.dcf.cashflow_parameters = ind
-pf.dcf.discount_rate = 0.10
-pf.dcf.use_discounted_values = True
+pf.dcf.cashflow_parameters = pc
+# pf.dcf.discount_rate = 0.10
+# pf.dcf.use_discounted_values = True
 
 # Set Monte Carlo
-pf.dcf.set_mc_parameters(distribution="t", period=50, number=100)
+pf.dcf.set_mc_parameters(distribution="norm", period=30, number=100)
 
-pf.dcf.plot_forecast_monte_carlo(backtest=True)
+w = pf.dcf.find_the_largest_withdrawals_size(
+    withdrawal_steps=10,   # The number of intermediate steps during the iteration of values fom max to min of the withdrawal size
+    confidence_level=0.50,  # Confidence level defines the percentile of Monte Carlo time series. The 25th percentile is a negative scenario.
+    goal="maintain_balance",  # The goal of the strategy in this case is to keep the portfolio's real balance
+)
+print(w)
 
-plt.yscale("log")  # log or linear
-plt.legend("")
-# plt.savefig('time_series.png')
-plt.show()
+# pf.dcf.plot_forecast_monte_carlo(backtest=True)
+#
+# plt.yscale("log")  # log or linear
+# plt.legend("")
+# # plt.savefig('time_series.png')
+# plt.show()
