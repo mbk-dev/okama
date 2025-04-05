ef2b36eef9ec70962f717d83f5ccd6bff458250b
Sergey Kikevich <sergey@rostsber.ru>
2025-04-05 16:50:41 +0300

    chore: add info about problem with EF optimization to main_notebook.ipynb and main.py

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ warnings.simplefilter(action="ignore", category=FutureWarning)
 
 pd.set_option("display.float_format", lambda x: "%.2f" % x)
 
+ef = ok.EfficientFrontierReb(['SPY.US', 'MCFTR.INDX'], ccy='RUB', last_date='2025-03', bounds=None, full_frontier=True)
+df1_reb_year = ef.ef_points
+
+fig, ax = plt.subplots(figsize=(12, 10))
+
+# Plot the Efficient Frontiers
+ax.plot(df1_reb_year.Risk, df1_reb_year.CAGR, label="Annually rebalanced")
+# ax.plot(df1_not_reb.Risk, df1_not_reb.CAGR, label="Not rebalanced")
+
+# Plot the assets
+ef.plot_assets(kind="cagr")
+plt.show()
+
 
 # portfolio = ['RUCBTRNS.INDX', 'RGBITR.INDX', 'MCFTR.INDX', 'GC.COMM' ] # Список активов
 # weights = [.22, .09, .45, .24]
@@ -26,39 +39,35 @@ pd.set_option("display.float_format", lambda x: "%.2f" % x)
 # print(pf.dividend_yield_annual)
 
 
-assets = ['RGBITR.INDX', 'RUCBTRNS.INDX', 'MCFTR.INDX', 'GC.COMM']
-weights = [0.16, 0.40,  0.25, 0.19]
-pf = ok.Portfolio(assets, weights=weights, ccy='RUB', rebalancing_period='year', inflation=False)
-pf.dcf.discount_rate = 0.09
-
-ind = ok.IndexationStrategy(pf)
-ind.initial_investment = 2_000_000 * 90
-ind.amount = -4000 * 90
-ind.frequency = "month"
-ind.indexation = 0.09
-
-pf.dcf.cashflow_parameters = ind
-
-pf.dcf.set_mc_parameters(
-    distribution="norm",
-    period=50,
-    number=1000
-)
+# assets = ['RGBITR.INDX', 'RUCBTRNS.INDX', 'MCFTR.INDX', 'GC.COMM']
+# weights = [0.16, 0.40,  0.25, 0.19]
+# pf = ok.Portfolio(assets, weights=weights, ccy='RUB', rebalancing_period='year', inflation=False)
+# pf.dcf.discount_rate = 0.09
+#
+# ind = ok.IndexationStrategy(pf)
+# ind.initial_investment = 2_000_000 * 90
+# ind.amount = -4000 * 90
+# ind.frequency = "month"
+# ind.indexation = 0.09
+#
+# pf.dcf.cashflow_parameters = ind
+#
+# pf.dcf.set_mc_parameters(
+#     distribution="norm",
+#     period=50,
+#     number=1000
+# )
 
 # df = pf.dcf.monte_carlo_survival_period()
 # print(df.describe())
 
-w, err = pf.dcf.find_the_largest_withdrawals_size(
-    goal="maintain_balance_pv",
-    percentile=20,
-    tolerance_rel=0.01,
-)
-
-print(w, err)
-
-
-
-
+# w, err = pf.dcf.find_the_largest_withdrawals_size(
+#     goal="maintain_balance_pv",
+#     percentile=20,
+#     tolerance_rel=0.01,
+# )
+#
+# print(w, err)
 
 
 
