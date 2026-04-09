59938471167a1aa3935e30f8bca336b4b6c84e8e
Sergey Kikevich <sergey@rostsber.ru>
2026-04-09 18:49:16 +0300

    chore: upgrade runtime Python version to 3.14

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import warnings
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import okama as ok
 
 import os
 
-os.environ["PYTHONWARNINGS"] = "ignore::FutureWarning"
-warnings.simplefilter(action="ignore", category=FutureWarning)
+# os.environ["PYTHONWARNINGS"] = "ignore::FutureWarning"
+# warnings.simplefilter(action="ignore", category=FutureWarning)
 
 pd.set_option("display.float_format", lambda x: "%.2f" % x)
 
 tickers = [
-    "VNQ.US",
-    "DBXD.XFRA",
-    "MCFTR.INDX",
+    "SPY.US",
+    "AGG.US",
+    "GC.COMM",
 ]  # we can create lists of assets and portfolio containing general type of assets and **indexes**
-w = [0.5, 0.25, 0.25]
+w = [0.7, 0.15, 0.15]
 currency = "USD"
 
-y = ok.Portfolio(tickers, ccy=currency, weights=w)
+y = ok.Portfolio(tickers, ccy=currency, weights=w, inflation=True)
 
-print(y)
+y.dcf.discount_rate = None
+y.dcf.wealth_index(discounting="pv", include_negative_values=False).plot()
+
+plt.show()
